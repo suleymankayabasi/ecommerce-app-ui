1af7820299e0605c62ddc7a178c010c817ca8ab6
Süleyman Kayabaşı <82765835+suleymankayabasi@users.noreply.github.com>
2022-01-09 21:39:11 +0300

    Initial Commit

---
+++ src/ProductList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Product extends Component {
+  render() {
+    return (
+      <div>
+        <h3>{this.props.info.title}</h3>
+      </div>
+    );
+  }
+}
